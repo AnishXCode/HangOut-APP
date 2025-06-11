aa4dc5ef1a1986637a50d320c1326d5833f2a35d
vishalreddy111006 <vishal_kr@cs.iitr.ac.in>
2025-06-11 16:02:43 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBmVO_odnaryiyi9jfsoGeoLHPZcakYww4",
+  authDomain: "hangout-app-40038.firebaseapp.com",
+  projectId: "hangout-app-40038",
+  storageBucket: "hangout-app-40038.firebasestorage.app",
+  messagingSenderId: "129868118855",
+  appId: "1:129868118855:web:6364c737ea93431f62ea62"
 };
 
 const app = initializeApp(firebaseConfig);
